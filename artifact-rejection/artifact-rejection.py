a60224ac36ef5cb7671a2d93259cd3d34e8ec142
Peter Yuan <peteryuan24@gmail.com>
2018-10-24 10:42:56 -0700

    Implement epoch array rejection comparison and epoch_3d_arr matrices

--- artifact-rejection/artifact-rejection.py
+++ artifact-rejection/artifact-rejection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from utilities import *
+from utils import *
 
 
 def main():
@@ -16,27 +16,17 @@ def main():
     files = load_subject_dir(file_path, mat_reject, mat_stage)
     epochs = files['epochs']
     try:
-        reject = files['reject']
+        rejects = files['rejects']
     except:
         pass
 
-    # Read epochs into dataframe and clean df
-    index, scaling_time, scalings = ['epoch', 'time'], 1e3, dict(grad=1e13)
-    df_read = epochs.to_data_frame(picks=None, scalings=scalings, scaling_time=scaling_time, index=index)
-    df = clean_df(df_read)
-    # df_ = df.copy()
+    epoch_3d_array = epochs.get_data()
 
-    # Select values from dataframe and reject file
-    df_values = extract_df_values(df)
-    X, y = df_values.values, reject
-    df_IF = run_IForest(X, y, df)
+    X, y = epoch_3d_array, rejects
+    y_pred = run_SVM(epoch_3d_array, rejects):
 
-    # Create reject array .csv file
-    y_pred_reject = reject_epochs(df, df_IF)
-    y_pred_= np.asarray(y_pred_reject)
-    y_pred_.tofile('y_pred.csv',sep=',',format='%1f')
-    acc_score = accuracy_score(reject, y_pred_)
-    print(acc_score)
+    # Create .csv file with rejected <#> epochs
+    y_pred.tofile('y_pred.csv', sep=',', format='%0f')
 
 
 if __name__ == "__main__":
