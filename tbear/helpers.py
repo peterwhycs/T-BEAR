c335be7dd7e6ccea94cbaa545d5ddeb312c792c5
Peter Yuan <peteryuan24@gmail.com>
2019-04-08 11:05:27 -0700

    Refactor and moved out nonhelper functions

--- tbear/helpers.py
+++ tbear/helpers.py
@@ -5,14 +5,10 @@
 """
 # =============================================================================
 
-from typing import Any
-
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 import scipy.io as sio
 from mne import read_epochs_eeglab, set_config, set_log_level
-from sklearn.model_selection import KFold, train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 RANDOM_STATE: int = 42
@@ -31,6 +27,12 @@ def load_reject_mat(abs_reject_path: str) -> np.ndarray:
     return reject_file
 
 
+def min_max_scale(dataset: np.ndarray) -> np.ndarray:
+    scaler = MinMaxScaler(feature_range=[0, 1])
+    scaled_data = scaler.fit_transform(dataset)
+    return scaled_data
+
+
 def reshape_data_2d(dataset: np.ndarray) -> np.ndarray:
     num_epochs, num_features, samples_per_epoch = dataset.shape
     dataset = dataset.reshape((num_epochs, num_features * samples_per_epoch))
@@ -41,40 +43,3 @@ def standard_scale(dataset: np.ndarray) -> np.ndarray:
     scaler = StandardScaler()
     scaled_data = scaler.fit_transform(dataset)
     return scaled_data
-
-
-def min_max_scale(dataset: np.ndarray) -> np.ndarray:
-    scaler = MinMaxScaler(feature_range=[0, 1])
-    scaled_data = scaler.fit_transform(dataset)
-    return scaled_data
-
-
-def visualize_pca_component(expl_var_ratio: np.ndarray) -> None:
-    # Plot the Cumulative Summation of the Explained Variance
-    plt.figure()
-    plt.plot(expl_var_ratio)
-    plt.xlabel("Number of Components")
-    plt.ylabel("Variance (%)")
-    plt.title("Explained Variance")
-    plt.show()
-
-
-def train_model_split(model: Any, X: np.ndarray, y: np.ndarray, random_state: int = RANDOM_STATE) -> Any:
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
-    model.fit(X_train, y_train)
-    test_score = model.score(X_test, y_train)
-    print(f"Test Score: {test_score}")
-    return model
-
-
-def train_model_kfold(model: Any, X: np.ndarray, y: np.ndarray, n_splits: int = 3,
-                      random_state: int = RANDOM_STATE) -> Any:
-    kf = KFold(n_splits=n_splits, random_state=random_state, shuffle=False)
-    kfold_scores = []
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        model.fit(X_train, y_train)
-        kfold_scores.append(model.score(X_test, y_test))
-    print(f"Test Scores: {kfold_scores}")
-    return model
