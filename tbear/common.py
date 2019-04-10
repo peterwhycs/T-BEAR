c83ac04d52adceae220cf666da5a45d572f94dd3
Peter Yuan <peteryuan24@gmail.com>
2019-04-10 13:08:05 -0700

    Debug train_model_split

--- tbear/common.py
+++ tbear/common.py
@@ -71,7 +71,7 @@ def pca_transform_default(dataset: np.ndarray,
 def train_model_split(model: Any, X: np.ndarray, y: np.ndarray, random_state: int = RANDOM_STATE) -> Any:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
     model.fit(X_train, y_train)
-    test_score = model.score(X_test, y_train)
+    test_score = model.score(X_test, y_test)
     print(f"Test Score: {test_score}")
     return model
 
